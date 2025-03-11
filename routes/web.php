f58591f8a0a271841f0bab82fe9abb07d148faf9
vijay8528 <sahnivijay8756@gmail.com>
2025-03-11 12:14:10 -0700

    vijay

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('front.index');
 // });
-
+Route::get('/text',function () {
+    return "y89i00p";
+});
 Route::get('/in',[HomeController::class,'in']);
 // Route::get('index', [HomeController::class, 'index'])->name('index');
 Route::get('/', [HomeController::class, 'index'])->name('index');
